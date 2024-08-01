1727d1994de185cd7e26f667e147efae06264438
mhmdnzr <mohammadnzrhelia@gmail.com>
2024-08-01 10:27:08 +0330

    feat: compress files with gulp

---
+++ IDPServer/gulpfile.js
@@ -0,0 +1,23 @@
+﻿const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+// Minify JavaScript
+gulp.task('scripts', function () {
+    return gulp.src('wwwroot/lib/**/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('wwwroot/lib'));
+});
+
+// Minify CSS
+gulp.task('styles', function () {
+    return gulp.src('wwwroot/css/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+// Default task - run only minification tasks
+gulp.task('default', gulp.series('scripts', 'styles'));
