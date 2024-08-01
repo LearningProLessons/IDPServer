d6bd55b188178d9a6a6bcb8884effb2ebc9024d4
mhmdnzr <mohammadnzrhelia@gmail.com>
2024-08-01 16:35:25 +0330

    migration

--- IDPServer/Data/Migrations/20240801121933_ConfigurationMigration.Designer.cs
+++ IDPServer/Data/Migrations/20240801121933_ConfigurationMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IDPServer.Data.Migrations
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    [Migration("20240801063139_ConfigurationMigration")]
+    [Migration("20240801121933_ConfigurationMigration")]
     partial class ConfigurationMigration
     {
         /// <inheritdoc />
